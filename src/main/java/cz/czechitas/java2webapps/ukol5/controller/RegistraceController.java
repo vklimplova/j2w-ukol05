1c5eab9e45e4c917ec379bc78ecee4cc9e55fbca
veruklimpl <veronika.klimplova@seznam.cz>
2023-11-06 20:05:03 +0100

    nefunguje návrat error hlášek nebo vrácení rekapitulace

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,17 +1,42 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Random;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
  */
 @Controller
 public class RegistraceController {
-
+  private final Random random = new Random();
   @GetMapping("/")
-  public String formular() {
-    return "formular";
+  public ModelAndView formular() {
+    ModelAndView modelAndView = new ModelAndView("/formular");
+    modelAndView.addObject("form", new RegistraceForm());
+    return modelAndView;
   }
 
+  @PostMapping("/")
+  public Object form(@Valid @ModelAttribute("form") RegistraceForm form, BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors()) {
+      return "/formular";
+    }
+    return new ModelAndView("/rekapitulace")
+            .addObject("jmeno", form.getJmeno())
+            .addObject("prijmeni", form.getPrijmeni())
+            .addObject("datumNarozeni", form.getDatumNarozeni())
+            .addObject("pohlavi", form.getPohlavi())
+            .addObject("turnus", form.getTurnus());
+
+  }
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceForm.java
@@ -0,0 +1,89 @@
+package cz.czechitas.java2webapps.ukol5.controller;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+public class RegistraceForm {
+
+    @NotBlank(message = "Jméno nesmí být prázdné.")
+    private String jmeno;
+    @NotBlank(message = "Příjmení nesmí být prázdné.")
+    private String prijmeni;
+    @NotNull(message = "Registrovat jde pouze děti ve věku 9-15.")
+    private LocalDate datumNarozeni;
+    @NotBlank(message = "Prosím, vyplňte pohlaví.")
+    private String pohlavi;
+    @NotBlank(message = "Vyberte jeden z turnusů")
+    private String turnus;
+    @Email
+    private String email;
+    @Size(min=9,max=9)
+    private String telefon;
+
+    private int vek;
+
+    public int getVek() {
+        return vek;
+    }
+
+    public void setVek(int vek) {
+        this.vek = vek;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public LocalDate getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public void setDatumNarozeni(LocalDate datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public String getPohlavi() {
+        return pohlavi;
+    }
+
+    public void setPohlavi(String pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+}
